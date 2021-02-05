d6c87f0bc79cd1c10d671506a830eba9af94fb73
rkorzhoff <r.korzhoff@gmail.com>
2021-02-05 23:32:54 +0300

    event listeneres were added to buttons. Header switcher realized

--- App.js
+++ App.js
@@ -1,30 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [title, setTitle] = useState('THIS IS HELLO-WORLD APP');
   return (
     <View style={styles.container}>
-      <Text style={styles.mainHeader}>THIS IS HELLO-WORLD APP</Text>
+      <Text style={styles.mainHeader} onPress={() => setTitle('some text here 1')}>
+        {title}
+      </Text>
       <View style={styles.elementsContainer}>
-        <View style={styles.element}>
-          <Text>some text here 1</Text>
-        </View>
-        <View style={styles.element}>
-          <Text>some text here 1</Text>
-        </View>
-        <View style={styles.element}>
-          <Text>some text here 1</Text>
-        </View>
-        <View style={styles.element}>
-          <Text>some text here 1</Text>
-        </View>
-        <View style={styles.element}>
-          <Text>some text here 1</Text>
-        </View>
-        <View style={styles.element}>
-          <Text>some text here 1</Text>
-        </View>
+        <Text style={styles.element} onPress={() => setTitle('some text here 1')}>
+          some text here 1
+        </Text>
+
+        <Text style={styles.element} onPress={() => setTitle('some text here 2')}>
+          some text here 2
+        </Text>
+
+        <Text style={styles.element} onPress={() => setTitle('some text here 3')}>
+          some text here 3
+        </Text>
+
+        <Text style={styles.element} onPress={() => setTitle('some text here 4')}>
+          some text here 4
+        </Text>
+
+        <Text style={styles.element} onPress={() => setTitle('some text here 5')}>
+          some text here 5
+        </Text>
+
+        <Text style={styles.element} onPress={() => setTitle('some text here 6')}>
+          some text here 6
+        </Text>
       </View>
       <StatusBar style='auto' />
     </View>
@@ -63,5 +71,8 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 5,
     textAlign: 'center',
+    '&:hover': {
+      background: '#000',
+    },
   },
 });
